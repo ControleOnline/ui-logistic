c76124a5a00b82937bc4f6704d397ea974fcabbc
Luiz Kim <luiz.kim@controleonline.com>
2024-07-25 19:16:44 -0300

    Update

--- src/components/Logistic/index.vue
+++ src/components/Logistic/index.vue
@@ -3,11 +3,9 @@
 </template>
 
 <script>
-
 import { mapActions, mapGetters } from "vuex";
-import DefaultTable from '@controleonline/ui-default/src/components/Default/DefaultTable.vue';
-import * as DefaultFiltersMethods from '@controleonline/ui-default/src/components/Default/Scripts/DefaultFiltersMethods';
-import Button from "@controleonline/ui-common/src/components/Categories/Button";
+import DefaultTable from "@controleonline/ui-default/src/components/Default/DefaultTable.vue";
+import * as DefaultFiltersMethods from "@controleonline/ui-default/src/components/Default/Scripts/DefaultFiltersMethods";
 
 export default {
   props: {
@@ -19,39 +17,43 @@ export default {
 
   components: {
     DefaultTable,
-    Button
   },
   created() {
     if (this.orderId) {
-      let columns = this.columns.map(column => {
+      let columns = this.columns.map((column) => {
         if (column.name === "order") {
           return { ...column, filter: false, externalFilter: false };
         }
         return { ...column, externalFilter: false };
       });
-      this.addFilter('order', this.orderId);
+      this.addFilter("order", this.orderId);
 
-      this.$store.commit(this.configs.store + '/SET_COLUMNS', columns);
+      this.$store.commit(this.configs.store + "/SET_COLUMNS", columns);
     }
   },
   data() {
     return {
-      context: 'logistic'
-    }
+      context: "logistic",
+    };
   },
   computed: {
     ...mapGetters({
-      myCompany: 'people/currentCompany',
+      myCompany: "people/currentCompany",
     }),
     filters() {
-      return this.$copyObject(this.$store.getters[this.configs.store + '/filters'])
+      return this.$copyObject(
+        this.$store.getters[this.configs.store + "/filters"]
+      );
     },
     columns() {
-      return this.$copyObject(this.$store.getters[this.configs.store + '/columns'])
+      return this.$copyObject(
+        this.$store.getters[this.configs.store + "/columns"]
+      );
     },
     configs() {
       return {
-        store: 'logistic',
+        store: "logistic",
+        categories: ["logistic"],
         add: true,
         filters: true,
         editable: true,
@@ -62,35 +64,27 @@ export default {
           destinationType: {
             filters: {
               context: this.context,
-              company: '/people/' + this.myCompany.id
-            }
+              company: "/people/" + this.myCompany.id,
+            },
           },
           originType: {
             filters: {
               context: this.context,
-              company: '/people/' + this.myCompany.id
-            }
+              company: "/people/" + this.myCompany.id,
+            },
           },
           status: {
             filters: {
-              context: this.context
-            }
-          }
+              context: this.context,
+            },
+          },
         },
-        components: {
-          headerActions: {
-            component: Button,
-            props: {
-              context: this.context
-            }
-          }
-        }
       };
     },
   },
 
   methods: {
-    ...DefaultFiltersMethods
-  }
+    ...DefaultFiltersMethods,
+  },
 };
 </script>
