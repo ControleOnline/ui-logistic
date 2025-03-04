242589a696be1d9fd8eeb429c4eb6cdb2d703719
Luiz Kim <luiz.kim@controleonline.com>
2025-03-04 01:31:26 -0300

    Update

--- src/store/logistic/index.js
+++ src/store/logistic/index.js
@@ -7,6 +7,8 @@ import Formatter from "@controleonline/ui-common/src/utils/formatter.js";
 export default {
   namespaced: true,
   state: {
+ item:{},
+items:[],
     resourceEndpoint: "order_logistics",
     isLoading: false,
     error: "",
