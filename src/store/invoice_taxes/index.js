72d91f426571d1d022079032f4af55bdf11e1bfc
luizkim <luizkim@users.noreply.github.com>
2026-08-29 18:53:48 -0300

    feat: invoice_taxes store for NF without CTE listing

---
+++ src/store/invoice_taxes/index.js
@@ -0,0 +1,25 @@
+import * as actions from '@controleonline/ui-default/src/store/default/actions';
+import * as getters from '@controleonline/ui-default/src/store/default/getters';
+import mutations from '@controleonline/ui-default/src/store/default/mutations';
+import {CTE_PENDING_COLUMNS} from '@controleonline/ui-logistic/src/shared/ctePendingInvoices';
+
+export default {
+  namespaced: true,
+  state: {
+    item: null,
+    items: [],
+    resourceEndpoint: 'invoice_taxes/without-cte',
+    isLoading: false,
+    isSaving: false,
+    error: '',
+    totalItems: 0,
+    summary: {},
+    filters: {},
+    reload: false,
+    add: false,
+    columns: CTE_PENDING_COLUMNS,
+  },
+  actions,
+  getters,
+  mutations,
+};
