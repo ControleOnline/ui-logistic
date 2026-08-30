c9753fe8648997b79f3c2585dc5f4800022325ec
luizkim <luizkim@users.noreply.github.com>
2026-08-29 21:11:26 -0300

    fix(#670): store invoice_tasks_processing no staging

---
+++ src/store/invoice_tasks_processing.js
@@ -0,0 +1,28 @@
+import * as actions from '@controleonline/ui-default/src/store/default/actions';
+import * as getters from '@controleonline/ui-default/src/store/default/getters';
+import mutations from '@controleonline/ui-default/src/store/default/mutations';
+
+export default {
+  namespaced: true,
+  state: {
+    item: null,
+    items: [],
+    resourceEndpoint: 'invoice_tasks',
+    isLoading: false,
+    error: '',
+    totalItems: 0,
+    summary: {},
+    filters: {taskType: 'cte_emission', 'status.realStatus': 'pending'},
+    reload: false,
+    add: false,
+    columns: [
+      {name: 'id', label: 'ID', isIdentity: true, editable: false},
+      {name: 'cfop', label: 'CFOP', editable: false},
+      {name: 'invoiceTotal', label: 'Total', type: 'money', editable: false},
+      {name: 'status', label: 'Status', editable: false},
+    ],
+  },
+  actions,
+  getters,
+  mutations,
+};
