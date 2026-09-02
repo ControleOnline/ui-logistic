0559cc762560dc87bb119d734c6e1cf134c74825
Luiz Kim <luizkim@users.noreply.github.com>
2026-09-01 23:28:32 -0300

    fix(fiscal): keep all emission wrappers on shared screen

---
+++ src/react/pages/fiscal/NfceEmitPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import FiscalEmitPage from './FiscalEmitPage';
+
+export default function NfceEmitPage() {
+  return <FiscalEmitPage documentType="nfce" title="Emitir NFC-e" model={65} />;
+}
---
+++ src/react/pages/fiscal/NfseEmitPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import FiscalEmitPage from './FiscalEmitPage';
+
+export default function NfseEmitPage() {
+  return <FiscalEmitPage documentType="nfse" title="Emitir NFS-e" model="NFSE" />;
+}
