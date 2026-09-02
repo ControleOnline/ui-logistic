92c95dd53eeff7c3cd651b181ece3c8f15166f13
Luiz Kim <luiz.kim@controleonline.com>
2026-09-01 21:03:23 -0300

    feat: align fiscal emission screens with CT-e

---
+++ src/react/pages/fiscal/NfeEmitPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import FiscalEmitPage from './FiscalEmitPage';
+
+export default function NfeEmitPage() {
+  return <FiscalEmitPage documentType="nfe" title="Emitir NF-e" model={55} />;
+}
---
+++ src/react/pages/fiscal/NfseEmitPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import FiscalEmitPage from './FiscalEmitPage';
+
+export default function NfseEmitPage() {
+  return <FiscalEmitPage documentType="nfse" title="Emitir NFSe" model={99} />;
+}
